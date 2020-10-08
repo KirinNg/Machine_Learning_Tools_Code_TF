4b3b29b2e9a2f4b6c1f349aeb2f47088493d206c
KirinNg <kirinng0709@gmail.com>
2020-10-08 11:08:38 +0800

    mutiGPU

--- code_for_buildup_training/train_method.py
+++ code_for_buildup_training/train_method.py
@@ -127,3 +127,33 @@ bn_moving_vars = [g for g in g_list if 'moving_mean' in g.name]
 bn_moving_vars += [g for g in g_list if 'moving_variance' in g.name]
 var_list += bn_moving_vars
 saver = tf.train.Saver(var_list=var_list, max_to_keep=5)
+
+
+# 获得空闲GPU pip install nvidia-ml-py3
+import os
+import pynvml
+
+pynvml.nvmlInit()
+def usegpu(need_gpu_count=1):
+    nouse = []
+    for index in range(pynvml.nvmlDeviceGetCount()):
+        # 这里的0是GPU id
+        handle = pynvml.nvmlDeviceGetHandleByIndex(index)
+        meminfo = pynvml.nvmlDeviceGetMemoryInfo(handle)
+        used = meminfo.used / meminfo.total
+        if used < 0.5:
+            nouse.append(index)
+    if len(nouse) >= need_gpu_count:
+        os.environ["CUDA_VISIBLE_DEVICES"] = ','.join(map(str, nouse[:need_gpu_count]))
+        return nouse[:need_gpu_count]
+    elif len(nouse) > 0:
+        os.environ["CUDA_VISIBLE_DEVICES"] = ','.join(map(str, nouse))
+        return len(nouse)
+    else:
+        return 0
+if __name__ == '__main__':
+    gpus = usegpu(need_gpu_count=2)
+    if gpus:
+        print("use gpu ok")
+    else:
+        print("no gpu is valid")
